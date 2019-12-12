0d5c837300242fecdc680a8670c1e50ec0db3592
Christopher ESTIQUE <christopher.estique@icloud.com>
2019-12-12 15:00:57 +0100

    Modification Informations

--- fiberScan/fiberScan/InformationsViewController.swift
+++ fiberScan/fiberScan/InformationsViewController.swift
@@ -8,17 +8,56 @@
 
 import UIKit
 
-class InformationsViewController: UIViewController {
+class InformationsViewController: UIViewController, UITableViewDataSource {
+    
+    var buttonIsSelected = false
+    
+    struct Information {
+        var category: String
+        var score: String
+    }
+    
+    let informations: [Information] = [Information(category: "Humain", score: "30/100"), Information(category: "Environnement", score: "32/100"), Information(category: "Santé", score: "31/100")]
     
     
-
+    // Produit principal
+    @IBOutlet weak var imageMainProduct: UIImageView!
+    @IBOutlet weak var nameMainProduct: UILabel!
+    @IBOutlet weak var noteMainProduct: UILabel!
+    
+    @IBOutlet weak var addFavorites: UIButton!
+        let image1 = UIImage(named: "empty") as UIImage?
+        let image2 = UIImage(named: "filled") as UIImage?
+    
+    
+    // TableView
+    @IBOutlet weak var tableViewMainProduct: UITableView!
+    
+    // Produits alternatifs
+    @IBOutlet weak var alternativeProducts: UILabel!
+    @IBOutlet weak var imageAlternativeProductLeft: UIImageView!
+    @IBOutlet weak var nameAlternativeProductLeft: UILabel!
+    @IBOutlet weak var scoreAlternativeProductLeft: UILabel!
+    @IBOutlet weak var imageAlternativeProductRight: UIImageView!
+    
+    @IBOutlet weak var nameAlternativeProductRight: UILabel!
+    @IBOutlet weak var scoreAlternativeProductRight: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableViewMainProduct.dataSource = self
+        }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return informations.count
+    }
 
+    
     /*
     MARK: - Navigation
 
@@ -28,5 +67,29 @@ class InformationsViewController: UIViewController {
     Pass the selected object to the new view controller.
     }
     */
-
+    
+    
+    
+    // Bouton Favoris
+    @IBAction func onOffButton(_ sender: Any) {
+        buttonIsSelected = !buttonIsSelected
+            if buttonIsSelected == true {
+                addFavorites.tintColor = .systemGreen
+            } else if buttonIsSelected == false {
+                addFavorites.tintColor = .systemBlue
+            }
+            saveBool(bool: buttonIsSelected)
+        }
+    
+    func saveBool(bool: Bool) {
+        if bool == true {
+            print("favorite added")
+            
+        } else if bool == false {
+            print("favorite remove")
+        }
+    }
+    
 }
+    
+    
