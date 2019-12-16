9b642917a935281c9f351c7d4e89e5f21cb25791
Natalia <nataliavegariveros@gmail.com>
2019-12-16 17:04:53 +0100

    add link to information page from history view add function to remove in button modify note (work in progress)

--- fiberScan/fiberScan/CollectionViewCell.swift
+++ fiberScan/fiberScan/CollectionViewCell.swift
@@ -12,12 +12,27 @@ class CollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var image: UIImageView!
     
+    @IBOutlet weak var buttonDel: UIButton!
+    
+    weak var viewController: HistoriqueViewController?
+
+    private var textile: Textile!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
     
+    @IBAction func deleteElement(_ sender: Any) {
+      viewController?.remove(textile: textile)
+        }
+    
     public func configure(with model: Textile) {
+        self.textile = model
         image.image = model.image
     }
-}
+
+
+    }
+
+
